114d2bf42c23c8011ffa475ab33057fa89c2f27c
GoloJoker <1255026907@qq.com>
2016-04-16 19:26:53 +0800

    Initial commit

---
+++ app/src/main/java/com/example/administrator/javacoding/Utilities/MyTextUtil.java
@@ -0,0 +1,12 @@
+package com.example.administrator.javacoding.Utilities;
+
+/**
+ * Created by Administrator on 2016/4/16.
+ */
+public class MyTextUtil {
+
+    public String getValue() {
+        return "@Ting Ting I Love You! ";
+
+    }
+}
